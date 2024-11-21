8277bf8f4567e1c9e8f9f0f16286a697b31552dd
BryanPlaces <eduardo.places@gmail.com>
2024-11-21 17:25:08 +0000

    Adjusting styles

--- src/components/Todos/TodoFilters.tsx
+++ src/components/Todos/TodoFilters.tsx
@@ -8,19 +8,19 @@ const TodoFilters = () => {
     <section className="container mx-auto mt-8">
       <div className="dark:bg-gray-800 font-semibold flex justify-center gap-4 rounded-md bg-white p-4 transiton-all duration-1000">
         <button
-          className={`${ filter === 'all' ? 'text-blue-500 hover:text-gray-400' : 'text-gray-400 hover:text-blue-500' }`}
+          className={`${ filter === 'all' ? 'text-blue-500' : 'text-gray-400 hover:text-blue-500' }`}
           onClick={ () => changeFilter('all') }
         >
           All
         </button>
         <button
-          className={`${ filter === 'active' ? 'text-blue-500 hover:text-gray-400' : 'text-gray-400 hover:text-blue-500' }`}
+          className={`${ filter === 'active' ? 'text-blue-500' : 'text-gray-400 hover:text-blue-500' }`}
           onClick={ () => changeFilter('active') }
         >
           Active
         </button>
         <button
-          className={`${ filter === 'completed' ? 'text-blue-500 hover:text-gray-400' : 'text-gray-400 hover:text-blue-500' }`}
+          className={`${ filter === 'completed' ? 'text-blue-500' : 'text-gray-400 hover:text-blue-500' }`}
           onClick={ () => changeFilter('completed') }
         >
           Completed
